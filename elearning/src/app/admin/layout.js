e148377e6ff6b6375d14e9051cfb4f65e38e652c
Tran Trong Hieu <tranhieu4345@gmail.com>
2024-08-08 16:38:53 +0700

    update admin mobile sidebar

--- elearning/src/app/admin/layout.js
+++ elearning/src/app/admin/layout.js
@@ -5,14 +5,16 @@ import styles from '../assets/css/Components/sidebar.module.css'
 import Header from '../components/AdminHeader/Header'
 import { Provider } from 'react-redux';
 import { store } from '@/app/redux/store';
-const layout = ({children}) => {
+import SidebarMobile from '../components/AdminSidebar/SidebarMobile'
+const layout = ({ children }) => {
   return (
     <Provider store={store}>
-        <Sidebar></Sidebar>
-        <div id={styles.main}>
-            <Header></Header>
-            {children}
-        </div>
+      <Sidebar></Sidebar>
+      <div id={styles.main}>
+        <Header></Header>
+        <SidebarMobile></SidebarMobile>
+        {children}
+      </div>
     </Provider>
   )
 }
