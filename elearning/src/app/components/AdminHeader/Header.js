762de0a27fcc83f5c48c38fc009dae66b51e931f
Tran Trong Hieu <tranhieu4345@gmail.com>
2024-08-09 22:39:48 +0700

    update header

--- elearning/src/app/components/AdminHeader/Header.js
+++ elearning/src/app/components/AdminHeader/Header.js
@@ -1,62 +1,110 @@
-"use client"
-import { useRouter } from 'next/navigation'
-import { getUserInfo, handleLogout } from '@/app/server/action/users'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import UserDropdown from '../components/UserDropdown/UserDropdown.js';
+import { getUserInfo } from '../server/action/users';
+import styles from '../assets/css/Layout/header.module.css';
+import btn from '../assets/css/Components/button.module.css';
+import { getCategoryCourse } from '../server/action/course';
+import { useRouter } from 'next/navigation';
 
-const Header = () => {
-    const [adminProfile, setAdminProfile] = useState();
+const Header = (props) => {
+    const { category } = props;
+    const [userLogin, setUserLogin] = useState(null);
+    const [categoryCourse, setCategoryCourse] = useState(category);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showDropdown, setShowDropdown] = useState(false);
     const router = useRouter();
 
     useEffect(() => {
-        getUserInfo().then(result => {
-            if (result.maLoaiNguoiDung == 'GV') {
-                setAdminProfile(result);
-            } else {
-                router.push('/not-found');
-            }
-        })
-    }, [])
+        getCategoryCourse().then(result => {
+            setCategoryCourse(result);
+        });
+    }, []);
 
     useEffect(() => {
-        const checkUser = async () => {
-            try {
-                const result = await getUserInfo();
+        getUserInfo()
+            .then(result => {
+                setUserLogin(result);
+            })
+            .catch(error => {
+                console.error('Error fetching user info:', error);
+            });
+    }, []);
 
-                if (result && result.maLoaiNguoiDung === 'GV') {
-                    setAdminProfile(result);
-                } else {
-                    router.push('/not-found');
-                }
-            } catch (error) {
-                console.error('Failed to fetch user info:', error);
-                router.push('/not-found');
-            }
-        };
+    // render ra đăng nhập hoặc tài khoản --
+    const renderLoginLink = () => {
+        if (userLogin) {
+            return <UserDropdown userLogin={userLogin} />;
+        } else {
+            return <Link className={`${btn.buttonBg1}`} href="/users/dangnhap" role="button">Đăng nhập</Link>;
+        }
+    };
 
-        checkUser();
-    }, [router]);
+    // Tìm kiếm
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if (searchTerm.trim()) {
+            router.push(`/timkiem/${searchTerm}`);
+        }
+    };
 
-    if (!adminProfile) {
-        return null;
-    }
+    // Toggle dropdown menu
+    const handleDropdownToggle = () => {
+        setShowDropdown(!showDropdown);
+    };
 
     return (
-        <div className='header-admin d-flex justify-content-end'>
-            <p className='d-flex align-items-center px-2 py-0'>Chào! {adminProfile.hoTen}</p>
-            <div className="dropdown">
-                <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://i.pravatar.cc?1" width={50} className='rounded-circle' alt="" />
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><Link className="dropdown-item" href="/admin/thongtinadmin">Cập nhật thông tin</Link></li>
-                    <li><a className="dropdown-item" href="#" onClick={
-                        handleLogout
-                    }>Đăng xuất</a></li>
-                </ul>
-            </div>
-        </div>
-    )
+        <header>
+            <nav className={`navbar navbar-expand-lg fixed-top ${styles.navbar}`}>
+                <div className="container">
+                    <Link className="navbar-brand" href="/">
+                        <img src='/image/logo.png' className="img-fluid" style={{ maxWidth: '200px', height: 'auto' }} alt="Logo" />
+                    </Link>
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse px-5" id="collapsibleNavId">
+                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <Link className={`nav-link active ${styles.navLink}`} href="/" aria-current="page">Trang chủ
+                                    <span className="visually-hidden">(current)</span></Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className={`nav-link ${styles.navLink}`} href='/khoahoc' aria-current="page">Khoá học</Link>
+                            </li>
+                            <li className={`nav-item dropdown ${styles.dropdown}`}>
+                                <a className={`nav-link dropdown-toggle ${styles.dropdownToggle}`} href="#" onClick={handleDropdownToggle} aria-haspopup="true" aria-expanded={showDropdown}>Danh mục khoá học</a>
+                                <div className={`dropdown-menu ${styles.dropdownMenu} ${showDropdown ? 'show' : ''}`} aria-labelledby="dropdownId">
+                                    {
+                                        categoryCourse?.map((item, index) => {
+                                            return <Link className={`dropdown-item ${styles.dropdownItem}`} href={`/danhmuckhoahoc/${item.maDanhMuc}`} key={index}>{item.tenDanhMuc}</Link>
+                                        })
+                                    }
+                                </div>
+                            </li>
+                        </ul>
+                        {/* Form search */}
+                        <form className="d-flex my-2 my-lg-0 me-auto" onSubmit={handleSearch}>
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="Tìm kiếm"
+                                value={searchTerm}
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                            <button className={`btn my-2 my-sm-0 ${btn.buttonSearch}`} type="submit">
+                                <i className="fa fa-search"></i>
+                            </button>
+                        </form>
+                        <div>
+                            {renderLoginLink()}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
